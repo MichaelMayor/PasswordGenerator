6bd7f109d5d61dcc0a152ae1862db86615c02884
MichaelMayor <michael.davidge.mayor@gmail.com>
2020-08-22 11:24:41 -0400

    finished code

--- script.js
+++ script.js
@@ -1,5 +1,53 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+//generate arrays to pull from
+var capLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowLetters =capLetters.toLowerCase()
+var specialChar = "~`!@#$%^&*()-_=+[{]};:'<,>.?/\"\\";
+var password = "";
+
+function generatePassword () {
+    //prompt password length
+    var passLength = parseInt(prompt("How many characters long should the password be? (Please enter a number between 8 and 128)"));
+    
+        //check password length is within bounds
+        if (passLength > 8 && passLength < 128){
+            //find password specifications
+            var passCapLetters = confirm("Include Capital Letters?");
+            var passNumbers = confirm("Include Numbers?");
+            var passSpecialChar = confirm ("Include Special Characters?");
+            
+            
+            
+            for (i=0 ; i < passLength; i++){
+                //make lower case option
+                var options = lowLetters[Math.floor(Math.random()*25)];
+                //uppercase option
+                if (passCapLetters){
+                 var options = options + capLetters[Math.floor(Math.random()*25)];
+                }
+                //numerical option
+                if (passNumbers) {
+                    var num = Math.floor(Math.random()*9);
+                    var options = options + num.toString();
+                }
+                //special character option
+                if (passSpecialChar) {
+                 var options = options + specialChar[Math.floor(Math.random()*29)];
+                }
+                //choose option at random
+                currentChar = options[Math.floor(Math.random()*options.length)];
+                //add current character to password
+                password = password.concat(currentChar);
+            
+            }
+        }
+        //alert for invalid password length
+        else{
+            alert("Invalid Password Length");
+        }
+ return password;
+}
 
 // Write password to the #password input
 function writePassword() {
